5321b04a9854d6c31efc041e983cb2b77bf59118
dtvabrand <marcobertolone@outlook.com>
2025-11-24 06:54:48 -0800

    Update trakt_lists.py

--- trakt_lists.py
+++ trakt_lists.py
@@ -45,6 +45,7 @@ SITES=[
 ]
 
 QIDS = [
+    ("The Christmas Ring", "Q136411457"),    
     ("A Breed Apart", "Q133845943"),
     ("A Christmas Mystery", "Q135236473"),
     ("A Hollywood Christmas", "Q125975842"),
